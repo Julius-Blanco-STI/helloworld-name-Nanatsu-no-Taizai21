973a44d708e8caa895436237b6c16423f63c27ca
Nanatsu-no-Taizai21 <58576019+Nanatsu-no-Taizai21@users.noreply.github.com>
2019-12-06 08:35:59 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+
+package main;
+
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+       String your_name = "James Ryan";
+       System.out.println("Hello World! " + your_name);
+    }
+    
 }
