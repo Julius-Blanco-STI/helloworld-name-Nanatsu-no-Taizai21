2b8602297496781358139245316bdbb539f117aa
Nanatsu-no-Taizai21 <58576019+Nanatsu-no-Taizai21@users.noreply.github.com>
2019-12-06 08:43:56 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-
 package main;
-
-
 public class Main {
-
     public static void main(String[] args) {
        String your_name = "James Ryan";
        System.out.println("Hello World! " + your_name);
     }
-    
 }
